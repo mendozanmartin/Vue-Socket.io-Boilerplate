b9aa5dcef3170cd30c718233c43caf32dcfab8aa
mendozanmartin <mendozanmartin@gmail.com>
2019-01-20 16:13:06 -0500

    initial commit

--- server.js
+++ server.js
@@ -23,12 +23,28 @@
 //
 // });
 
-// server.js
-var express = require('express');
-var path = require('path');
+
+var express = require('express')
+  , http = require('http');
+//make sure you keep this order
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 var serveStatic = require('serve-static');
-app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+
+
+io.on('connection', (socket) => {
+  console.log(socket.id)
+  socket.on('SEND_MESSAGE', function(data) {
+      io.emit('MESSAGE', data);
+      console.log(data.message);
+});
+});
+
+server.listen(app.get('port'), function(){
+  console.log('listening on *:5000');
+});
